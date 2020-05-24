4818e3b4896d4aad6eb496518896418da66dcc0c
Ivan Derkach <theone@yandex-team.ru>
2020-05-24 18:15:01 +0300

    feat app: add first logic

--- src/main_window.py
+++ src/main_window.py
@@ -8,3 +8,19 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self._setup_events()
+
+    def _setup_events(self):
+        self.ui.GenerateCodeButton.clicked.connect(self.GenerateCode)
+
+    def GenerateCode(self):
+        info_mbox = QtWidgets.QMessageBox(self)
+        info_mbox.setIcon(QtWidgets.QMessageBox.Information)
+        info_mbox.setText(
+            """
+            Hello, I see you want to generate CFD code =)
+            I really want it too!
+            Hope you'll do it in the next version of application.
+            """
+        )
+        info_mbox.show()
