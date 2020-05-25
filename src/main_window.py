bde43e42c45f8ee98b1133e0f758af236ec2a507
Ivan Derkach <theone@yandex-team.ru>
2020-05-25 09:45:08 +0300

    feat app: problem model filled

--- src/main_window.py
+++ src/main_window.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from generated.UI import Ui_MainWindow
 
@@ -13,6 +13,47 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self._setup_events()
+
+    @property
+    def Equation(self) -> str:
+        return self.ui.EquationText.toPlainText()
+
+    @property
+    def CoordinateSystemSelected(self) -> Optional[models.CoordinateSystem]:
+        current_cs = self.ui.CoordinateSystemCombo.currentText()
+        if not current_cs:
+            return None
+        return models.CoordinateSystem.from_str(current_cs)
+
+    @property
+    def DimensionCount(self):
+        return int(self.ui.DimensionsCountSpin.value())
+
+    @property
+    def IsStationary(self):
+        return bool(self.ui.IsStationaryCheckBox.isChecked())
+
+    @property
+    def InitialCondition(self) -> Optional[str]:
+        if not self.ui.InitialConditionGroup.isVisible:
+            return None
+        return self.ui.InitialConditionText.toPlainText()
+
+    @property
+    def BoundaryConditionsLR(self):
+        conditions: List[QtWidgets.QGroupBox] = [
+            widget
+            for widget in self.ui.BoudaryConditionsGroup.children()
+            if type(widget) is not QtWidgets.QGridLayout
+        ]
+        L, R = [], []
+        for cond_group in conditions:
+            cond_text = GetTextFromGroup(cond_group)
+            if "левый" in cond_group.title():
+                L.append(cond_text)
+            elif "правый" in cond_group.title():
+                R.append(cond_text)
+        return L, R
 
     def _setup_events(self):
         self.ui.GenerateCodeButton.clicked.connect(self.GenerateCode)
@@ -51,25 +92,19 @@ class MainWindow(QtWidgets.QMainWindow):
             coinditions.addWidget(widget, *(i // 2, i % 2))
 
     def SetUpBoundaryConditions(self):
-        current_cs = self.ui.CoordinateSystemCombo.currentText()
-        if not current_cs:
-            return
-
-        coordinate_system = models.CoordinateSystem.from_str(current_cs)
-        self._GenConditionsPlaceHolders(coordinate_system)
+        if self.CoordinateSystemSelected:
+            self._GenConditionsPlaceHolders(self.CoordinateSystemSelected)
+        self.ui.InitialConditionGroup.setVisible(not self.IsStationary)
 
     def SetUpCoordinateSystemCombo(self):
-        dimension_count = int(self.ui.DimensionsCountSpin.value())
-        is_stationary = bool(self.ui.IsStationaryCheckBox.isChecked())
-
         coordinate_systems = models.CoordinateSystem.filtered(
-            dimension_count, is_stationary
+            self.DimensionCount, self.IsStationary
         )
         if len(coordinate_systems) == 0:
-            if dimension_count == 0:
+            if self.DimensionCount == 0:
                 self.ui.DimensionsCountSpin.setValue(1)
             else:
-                self.ui.DimensionsCountSpin.setValue(dimension_count - 1)
+                self.ui.DimensionsCountSpin.setValue(self.DimensionCount - 1)
             return
 
         self.ui.CoordinateSystemCombo.clear()
@@ -87,5 +122,22 @@ class MainWindow(QtWidgets.QMainWindow):
         )
         info_mbox.show()
 
-        equation_text: str = self.ui.EquationText.toPlainText()
-        print(equation_text)
+        if not self.CoordinateSystemSelected:
+            return
+
+        problem_model = models.ProblemModel(
+            equation=self.Equation,
+            L_boundary_conditions=self.BoundaryConditionsLR[0],
+            R_boundary_conditions=self.BoundaryConditionsLR[1],
+            initial_condition=self.InitialCondition,
+            dimensions_count=self.DimensionCount,
+            is_stationary=self.IsStationary,
+            coordinate_system=self.CoordinateSystemSelected,
+        )
+
+
+def GetTextFromGroup(group: QtWidgets.QGroupBox):
+    for child in group.children():
+        if hasattr(child, "toPlainText"):
+            return child.toPlainText()
+    return None
